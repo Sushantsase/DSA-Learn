f276cd49d3db0e08ec974504809d48577b6fd19f
Sushantsase <42954640+Sushantsase@users.noreply.github.com>
2024-09-23 16:01:13 +0530

    Update RemoveDuplicateElement.java

--- src/array/basic/RemoveDuplicateElement.java
+++ src/array/basic/RemoveDuplicateElement.java
@@ -4,6 +4,13 @@ public class RemoveDuplicateElement {
     public static void removedyplicate(int[] arr){
       //  int[] ans =
     }
+    public static void removeDuplicates(int[] a)
+    {
+        LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+        for (int i = 0; i < a.length; i++)
+            set.add(a[i]);
+        System.out.print(set);
+    }
     public static void main(String[] args) {
         int[] arr = new int[]{2,5,5,8,5,6,2,8,2,5,9};
         removedyplicate(arr);
